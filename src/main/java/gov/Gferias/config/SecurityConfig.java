af02437a6969e5ae5df619a8b9701eaee3506d18
jairolessa <jairo.rafaelcarvalho@gmail.com>
2026-04-16 16:17:07 -0300

    Feat (Config): implementando spring security

---
+++ src/main/java/gov/Gferias/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package gov.Gferias.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
